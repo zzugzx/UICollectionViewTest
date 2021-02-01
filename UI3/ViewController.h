e99324628e9a2ec5ec4c176a7e5ce7171af2b0ba
guozhengxing <guozhengxing@bytedance.com>
2021-02-01 16:17:31 +0800

    Initial Commit

---
+++ UI3/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  UI3
+//
+//  Created by bytedance on 2021/2/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI3/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  UI3
+//
+//  Created by bytedance on 2021/2/1.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
